71de34273f5935170498b7aa6dee0ad3548eff9e
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2020-04-21 09:40:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,11 @@ const client = new Discord.Client();
 const fs = require("fs");
  
 client.on('ready', () => {
-console.log('Bot: Hosting ' + `${client.users.size}` + ' utilisateurs, dans ' + `${client.channels.size}` + ' et ' + `${client.guilds.size}` + ' guildes.');
+console.log('Bot: Hosting ' + `${client.users.size}` + ' utilisateurs, dans ' + `${client.channels.size}` + 'salons et ' + `${client.guilds.size}` + ' serveurs.');
     client.user.setStatus('do not disturb')
     client.user.setPresence({
         game: {
-            name: '/meme | by SonMaxime',
+            name: '.level | by SonMaxime',
             type: "STREAMING",
             imageUrl: "https://www.heroku.com/"
         }
@@ -19,7 +19,7 @@ client.on('message', message => {
 var userData = JSON.parse(fs.readFileSync('jsonPointID/level.json', 'utf-8'));   
 var sender = message.author;
 var msg = message.content.toUpperCase();
-var prefix = '/'
+var prefix = '.'
  
  
 if(msg === prefix + 'level') {
